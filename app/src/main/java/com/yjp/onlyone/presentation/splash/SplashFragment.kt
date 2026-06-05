4a293ffc4c59474f70aeab733e6e99f77964f9fc
yunjung park <yunjj21@gmail.com>
2026-06-05 14:32:03 +0900

    Splash 화면 백 버튼 동작 구현

--- app/src/main/java/com/yjp/onlyone/presentation/splash/SplashFragment.kt
+++ app/src/main/java/com/yjp/onlyone/presentation/splash/SplashFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -32,6 +33,12 @@ class SplashFragment : BaseFragment<FragmentSplashBinding>() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        requireActivity().onBackPressedDispatcher.addCallback(
+            viewLifecycleOwner,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() = Unit
+            },
+        )
         binding.splashComposeView.setThemeContent {
             SplashScreen()
         }
