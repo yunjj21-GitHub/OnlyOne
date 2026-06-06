51ecf2a2e64d49a47be9c991e023a1418a8ef65d
yunjung park <yunjj21@gmail.com>
2026-06-06 17:41:06 +0900

    로딩 로티 추가 (사용처 없음)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -72,6 +72,7 @@ dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.lottie.compose)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
